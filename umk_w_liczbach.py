0153b57d871d3b752b9655c331fa99e7dd45f193
tomizi <106354750+tomizi@users.noreply.github.com>
2022-10-18 13:45:56 +0200

    Update umk_w_liczbach.py

--- umk_w_liczbach.py
+++ umk_w_liczbach.py
@@ -7,15 +7,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 from pathlib import Path
 
-page_bg_img = """
-<style>
-[data-testid="stAppViewContainer"]{
-background-color #0050AA;
-}
-</style>
-"""
 
-st.markdown(page_bg_img, unsafe_allow_html=True)
 
 
 st.set_page_config(page_title='UMK w liczbach', page_icon = ':page_facing_up:', layout='wide')
