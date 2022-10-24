de2cbc321cd0bf8a9ff81cf823da0d938e4c9453
tomizi <106354750+tomizi@users.noreply.github.com>
2022-10-24 15:54:04 +0200

    Update umk_w_liczbach.py

--- umk_w_liczbach.py
+++ umk_w_liczbach.py
@@ -47,7 +47,7 @@ if sekcja == 'Wydziały':
                                                 'Nauk o Polityce i Bezpieczeństwie','Prawa i Administracji','Sztuk Pięknych','Teologiczny','Lekarski',
                                                 'Farmaceutyczny','Nauk o Zdrowiu','Ogółem'])
     kat1 = st.selectbox('Wybierz kategorię: ', ['Stacjonarne','Niestacjonarne','Razem'])
-    st.plotly_chart(px.bar(DF3,x='Rok',y='Razem'))
+    st.plotly_chart(px.bar(DF3[DF3['Wydział']==wydzial,x='Rok',y=kat1))
 if sekcja == 'Granty':
     st.title('Granty')
 
