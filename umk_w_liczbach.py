c0535cddbfee67a70706fc7276d41eddc26fdb37
tomizi <106354750+tomizi@users.noreply.github.com>
2022-11-02 13:52:53 +0100

    Update umk_w_liczbach.py

--- umk_w_liczbach.py
+++ umk_w_liczbach.py
@@ -68,7 +68,7 @@ hide_st_style = """
             #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
             header {visibility: hidden;} 
-            [class="styles_terminalButton__3xUnY" ] {visibility: hidden;}
+            [data-testid="manage-app-button" ] {visibility: hidden;}
             
             </style>
             """
