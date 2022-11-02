0883db6e58b3f52db73414fca5e16d768b98a583
tomizi <106354750+tomizi@users.noreply.github.com>
2022-11-02 16:37:11 +0100

    Update umk_w_liczbach.py

--- umk_w_liczbach.py
+++ umk_w_liczbach.py
@@ -86,8 +86,8 @@ st.markdown(
     """
 <style>
 [data-testid="stAppViewContainer"]{background-image:url("https://www.historia.umk.pl/panel/wp-content/uploads/siw_logo_umk_pion.png");
-background-size:15%;
-background-position: 98% 1%;
+background-size:10%;
+background-position: 96% 2%;
 background-repeat: no-repeat;
 background-attachment: local;}
 [data-testid="stHeader"]{background-color: rgba(0,0,0,0);}
