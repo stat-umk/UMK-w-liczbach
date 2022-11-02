61a20f0a662c379e1f92c8eb2b12844790975228
tomizi <106354750+tomizi@users.noreply.github.com>
2022-11-02 12:45:16 +0100

    Update umk_w_liczbach.py

--- umk_w_liczbach.py
+++ umk_w_liczbach.py
@@ -77,8 +77,8 @@ st.markdown(
     """
 <style>
 .sidebar .sidebar-content {
-    background-image: linear-gradient(#2e7bcf,#2e7bcf);
-    color: blue;
+    background-image: linear-gradient(#8993ab,#8993ab);
+    color: white;
 }
 </style>
 """,
