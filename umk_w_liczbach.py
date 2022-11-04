0ca51ff2e2fd894c09fef869d1e09288afd17c8f
tomizi <106354750+tomizi@users.noreply.github.com>
2022-11-04 12:23:11 +0100

    Update umk_w_liczbach.py

--- umk_w_liczbach.py
+++ umk_w_liczbach.py
@@ -39,7 +39,7 @@ sekcja = st.sidebar.radio(
 st.markdown(
     """
 <style>
-[data-testid="stAppViewContainer"] > .main {background-image: url("https://ppshypothesis.umk.pl/pliki/logo%20UMK%20poziom%20ENG%20RGB.jpg");
+[data-testid="stAppViewContainer"] > .main {background-image: url("https://vetkompleksowo.pl/wp-content/uploads/sites/3/2020/07/logo-umk-miniatura.png");
 background-size:20%;
 background-position: 20% 6%;
 background-repeat: no-repeat;
