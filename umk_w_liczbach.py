2e160edf5ee47474e7336d1bad5fa8aa43cf9f60
tomizi <106354750+tomizi@users.noreply.github.com>
2022-11-05 11:53:06 +0100

    Update umk_w_liczbach.py

--- umk_w_liczbach.py
+++ umk_w_liczbach.py
@@ -95,13 +95,13 @@ if sekcja == 'Studenci':
         marker_line_color='rgb(0,70,180)',marker_line_width=1.5).update_xaxes(title_font=dict(size=18)).update_yaxes(title_font=dict(size=18)))
     with c2:
         st.subheader('Studia niestacjonarne')
-        wydzial1 = st.selectbox('Wybierz wydział: ',wydziały)
+        wydzial1 = st.selectbox('Wybierz wydział : ',wydziały)
         #kat1 = st.selectbox('Wybierz kategorię: ', ['Stacjonarne','Niestacjonarne','Razem'])
         st.plotly_chart(px.bar(DF3[DF3['Wydział']==wydzial1],x='Rok',y='Niestacjonarne',width=600,height=400).update_traces(marker_color=kolwyd[wydzial],texttemplate="%{y:}",textposition='inside',
         marker_line_color='rgb(0,70,180)',marker_line_width=1.5).update_xaxes(title_font=dict(size=18)).update_yaxes(title_font=dict(size=18)))
     with c3:
         st.subheader('Razem')   
-        wydzial2 = st.selectbox('Wybierz wydział: ',wydziały)
+        wydzial2 = st.selectbox('Wybierz wydział  : ',wydziały)
         st.plotly_chart(px.bar(DF3[DF3['Wydział']==wydzial2],x='Rok',y='Razem',width=600,height=400).update_traces(marker_color=kolwyd[wydzial],texttemplate="%{y:}",textposition='inside',
         marker_line_color='rgb(0,70,180)',marker_line_width=1.5).update_xaxes(title_font=dict(size=18)).update_yaxes(title_font=dict(size=18)))
         
