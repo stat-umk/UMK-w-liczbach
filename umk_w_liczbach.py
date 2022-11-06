b5db4526a2ca2ebe8492f5068ae52c2b9898166b
tomizi <106354750+tomizi@users.noreply.github.com>
2022-11-06 11:30:30 +0100

    Update umk_w_liczbach.py

--- umk_w_liczbach.py
+++ umk_w_liczbach.py
@@ -183,7 +183,7 @@ if sekcja == 'Nauczyciele akademiccy i administracja':
         st.plotly_chart(fig)
     ck1,ck2 = st.columns(2)
     wydział = ck1.selectbox("Wybierz wydział:",wydziały)
-    wydział1 = ck2.selectbox("Wybierz wydział:",wydziały)
+    wydział1 = ck2.selectbox("Wybierz wydział: ",wydziały)
     fig = px.line(DF5[DF5['Jednostka Organizacyjna']==wydział],x='Rok',
 		  y='Liczba nauczycieli akademickich',markers=True).update_traces(marker_color=('rgb(0,80,170)'),
 		  line_color=(kolwyd[wydział])).update_yaxes(tickformat=",").update_layout(font_family='Lato')
