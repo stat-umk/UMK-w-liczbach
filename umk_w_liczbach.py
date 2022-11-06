d4e9247933422de54f9261aeab86c9c0773a254b
tomizi <106354750+tomizi@users.noreply.github.com>
2022-11-06 22:25:39 +0100

    Update umk_w_liczbach.py

--- umk_w_liczbach.py
+++ umk_w_liczbach.py
@@ -191,12 +191,12 @@ if sekcja == 'Nauczyciele akademiccy i administracja':
     ck1,ck2 = st.columns(2)
     wydział = ck1.selectbox("Wybierz wydział:",DF5['Jednostka Organizacyjna'].unique()[:-5])
     wydział1 = ck2.selectbox("Wybierz wydział: ",DF5['Jednostka Organizacyjna'].unique()[:-5][DF5[DF5['Jednostka Organizacyjna'] == wydział].index.tolist()[0]+1:])
-    fig = px.line(DF5[DF5['Jednostka Organizacyjna'].isin([wydział,wydział1])],x='Rok',
+    fig = px.line(DF5[DF5['Jednostka Organizacyjna'].isin([wydział,wydział1])].sort_values(by='Rok'),x='Rok',
 		  y='Liczba nauczycieli akademickich',color='Jednostka Organizacyjna',width=1400,height=500,
 		  markers=True,color_discrete_sequence=[kolwyd[wydział], kolwyd[wydział1]],text='Liczba nauczycieli akademickich').update_traces(marker_color=('rgb(0,80,170)'),
 		  textposition="top right").update_yaxes(tickformat=",").update_layout(font_family='Lato')
     st.plotly_chart(fig)
-    st.dataframe(DF5[DF5['Jednostka Organizacyjna'].isin([wydział,wydział1])])  
+    st.dataframe(DF5[DF5['Jednostka Organizacyjna'].isin([wydział,wydział1])].sort_values(by='Rok'))  
       
       
 
