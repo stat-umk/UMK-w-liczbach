6d9c81d4dfc9139632c38a25e78cc9749906814f
tomizi <106354750+tomizi@users.noreply.github.com>
2022-11-06 10:07:46 +0100

    Update umk_w_liczbach.py

--- umk_w_liczbach.py
+++ umk_w_liczbach.py
@@ -213,9 +213,9 @@ if sekcja == 'Badania naukowe':
     fig.add_trace(go.Bar(x=y,y=x,orientation='h',text=y,
                         textfont=dict( size=10,color='black')))
     fig.update_traces(marker_color=barwa,marker_line_color='black',marker_line_width=1.5,
-                      textposition='outside',texttemplate = "<b>%{x:}",textfont={'size':15,'family':'Lato'})
-    fig.update_xaxes(title='Kwota wnioskowana[zł]')
-    fig.update_yaxes(title='Jednostka')
+                      textposition='outside',texttemplate = "<b>%{x:}",textfont={'family':'Lato'})
+    fig.update_xaxes(title='Kwota wnioskowana[zł]',textfont={'family':'Lato'})
+    fig.update_yaxes(title='Jednostka',textfont={'family':'Lato'})
 
     fig.update_layout(xaxis=dict(showline=False,showgrid=True,showticklabels=True,linewidth=2,linecolor='black',gridwidth=1,gridcolor='gray',mirror=True),
                                 height=600,width=1600,plot_bgcolor='white',margin=dict(t=100, b=100, l=0, r=200),
