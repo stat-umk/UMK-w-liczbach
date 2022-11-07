6fbb20c32b97b10fa11d8f0c6bebaec2a31fa2fc
tomizi <106354750+tomizi@users.noreply.github.com>
2022-11-07 10:35:10 +0100

    Update umk_w_liczbach.py

--- umk_w_liczbach.py
+++ umk_w_liczbach.py
@@ -48,7 +48,7 @@ sekcja = st.sidebar.radio(
     'Wybierz sekcję:',
     ('Strona główna','Studenci','Nauczyciele akademiccy i administracja','Badania naukowe','Współpraca międzynarodowa')
  )
-st.sidebar.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThBBfnP58vXoV89jp6geeU5ciwdqWu6E8IbQ&usqp=CAU', use_column_width=True)
+#st.sidebar.image('', use_column_width=True)
 
 streamlit_style = """
 			<style>
