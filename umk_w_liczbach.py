38a813d286465fa8badace833f7e2505b9ef121b
tomizi <106354750+tomizi@users.noreply.github.com>
2022-11-08 13:20:28 +0100

    Update umk_w_liczbach.py

--- umk_w_liczbach.py
+++ umk_w_liczbach.py
@@ -72,7 +72,7 @@ st.markdown(
     """
 <style>
 [data-testid="stAppViewContainer"] > .main {background-image: url("https://login.umk.pl/themes/umk/images/logo-umk.png");
-background-size:30%;
+background-size:200px,200px;
 background-position: 1200px 50px;
 background-repeat: no-repeat;
 background-attachment: local;}
