e860306b553188cdcbfe04801cc9f3e4b4a1a65a
tomizi <106354750+tomizi@users.noreply.github.com>
2023-01-09 22:01:51 +0100

    Update umk_w_liczbach.py

--- umk_w_liczbach.py
+++ umk_w_liczbach.py
@@ -23,6 +23,7 @@ DF5 = pd.read_excel(io='Studenci.xlsx',engine='openpyxl',sheet_name='nauczyciele
 DF6 = pd.read_excel(io='Studenci.xlsx',engine='openpyxl',sheet_name='Granty_przyznane',dtype={'Rok':int})
 
 DF7 = pd.read_excel(io='Studenci.xlsx',engine='openpyxl',sheet_name='L_kier_stud',dtype={'Rok':str})
+DF8 = pd.read_excel(io='Studenci.xlsx',engine='openpyxl',sheet_name='N-wni',dtype={'Rok':int})
 #DF7 = pd.read_excel(io='Studenci.xlsx',engine='openpyxl',sheet_name='Wyjazdy',dtype={'Rok':int})
 #DF8 = pd.read_excel(io='Studenci.xlsx',engine='openpyxl',sheet_name='Przyjazdy')
 
@@ -131,9 +132,15 @@ if sekcja == 'Studenci':
     st.header('Liczba kierunków studiów w latach 2009-2021')
     st.plotly_chart(px.bar(DF7,x='Rok',y='Liczba',width=1050,height=400).update_traces(marker_color='rgb(0,80,170)',texttemplate="%{y:}",
 	textposition='inside',marker_line_color='rgb(0,70,180)',marker_line_width=2.5)
-			.update_xaxes(title_font=dict(size=12), title='Lata').update_yaxes(title_font=dict(size=12),title = 'Liczba kierunków').update_layout(font_family='Lato'))
+	.update_xaxes(title_font=dict(size=12), title='Lata').update_yaxes(title_font=dict(size=12),title = 'Liczba kierunków').update_layout(font_family='Lato'))
 	
+	
+    st.header('Odsetek studentów niepełnosprawnych w latach 2012-2021 w podziale na rodzaj studiów')
+    st.plotly_chart(px.line(DF8,x='Rok',y='Liczba',width=1050,height=400).update_traces(marker_color='rgb(0,80,170)',texttemplate="%{y:}",
+	textposition='inside',marker_line_color='rgb(0,70,180)',marker_line_width=2.5)
+	.update_xaxes(title_font=dict(size=12), title='Lata').update_yaxes(title_font=dict(size=12),title = 'Liczba kierunków').update_layout(font_family='Lato'))
     st.header('Liczba studentów i absolwentów studiów stacjonarnych i niestacjonarnych w latach 2019-2021 na poszczgólnych wydziałach')
+
     c1, c2, c3 = st.columns(3)
     with c1:
         st.subheader('Studia stacjonarne')   
