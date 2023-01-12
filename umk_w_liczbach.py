85182d2a963de49b4affa03df9b6b8de4839d2ee
tomizi <106354750+tomizi@users.noreply.github.com>
2023-01-12 10:04:01 +0100

    Update umk_w_liczbach.py

--- umk_w_liczbach.py
+++ umk_w_liczbach.py
@@ -162,12 +162,13 @@ if sekcja == 'Studenci':
     fig = px.bar(DF11[DF11['Wydział']==wydzial34],x='Rok',y='Liczba',width=550,height=400).update_traces(marker_color=kolwyd[wydzial],
     texttemplate="%{y:}",textposition='inside',
     marker_line_color='rgb(0,70,180)',marker_line_width=2.5).update_layout(font_family='Lato')
+    fig2 = px.bar(DF12,x='Rok',y='Liczba',width=550,height=400).update_traces(marker_color='rgb(0,70,180)',
+    texttemplate="%{y:}",textposition='inside',
+    marker_line_color='rgb(0,70,180)',marker_line_width=2.5).update_layout(font_family='Lato')
     if kat34 == 'Doktoranckie':
         st.plotly_chart(fig)
     elif kat34 == 'Podyplomowe':
-	st.plotly_chart(px.bar(DF12,x='Rok',y='Liczba',width=550,height=400).update_traces(marker_color='rgb(0,70,180)',
-	texttemplate="%{y:}",textposition='inside',
-        marker_line_color='rgb(0,70,180)',marker_line_width=2.5).update_layout(font_family='Lato'))
+	st.plotly_chart(fig2)
         
   
 
