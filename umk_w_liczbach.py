3c4e36fac5acdf9caf29ac4afb66c74550e4d6ff
tomizi <106354750+tomizi@users.noreply.github.com>
2023-01-12 10:02:55 +0100

    Update umk_w_liczbach.py

--- umk_w_liczbach.py
+++ umk_w_liczbach.py
@@ -158,10 +158,12 @@ if sekcja == 'Studenci':
     q1, q2 = st.columns(2)
     kat34 = q1.selectbox('Wybierz kategorię : ',['Studia wyższe stacjonarne','Studia wyższe niestacjonarne','Doktoranckie','Podyplomowe','Razem'])
     wydzial34 = q2.selectbox('Wybierz wydział : ',DF11['Wydział'])
+	
+    fig = px.bar(DF11[DF11['Wydział']==wydzial34],x='Rok',y='Liczba',width=550,height=400).update_traces(marker_color=kolwyd[wydzial],
+    texttemplate="%{y:}",textposition='inside',
+    marker_line_color='rgb(0,70,180)',marker_line_width=2.5).update_layout(font_family='Lato')
     if kat34 == 'Doktoranckie':
-        st.plotly_chart(px.bar(DF11[DF11['Wydział']==wydzial34],x='Rok',y='Liczba',width=550,height=400).update_traces(marker_color=kolwyd[wydzial],
-	texttemplate="%{y:}",textposition='inside',
-        marker_line_color='rgb(0,70,180)',marker_line_width=2.5).update_layout(font_family='Lato'))
+        st.plotly_chart(fig)
     elif kat34 == 'Podyplomowe':
 	st.plotly_chart(px.bar(DF12,x='Rok',y='Liczba',width=550,height=400).update_traces(marker_color='rgb(0,70,180)',
 	texttemplate="%{y:}",textposition='inside',
