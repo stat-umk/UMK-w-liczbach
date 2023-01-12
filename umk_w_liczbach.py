3316ac2a2020a121c4a6c401d62a372960d552ce
tomizi <106354750+tomizi@users.noreply.github.com>
2023-01-12 13:22:34 +0100

    Update umk_w_liczbach.py

--- umk_w_liczbach.py
+++ umk_w_liczbach.py
@@ -146,7 +146,7 @@ if sekcja == 'Studenci':
     q1, q2 = st.columns(2)
     kat34 = q1.selectbox('Wybierz kategorię : ',['Studia stacjonarne','Studia niestacjonarne','Doktoranckie','Podyplomowe','Ogółem'])
 
-    st.write(DF11[DF11['Wydział']=='Chemii'])
+    st.write(np.max(DF11[DF11['Wydział']=='Chemii']['Liczba]))
     fig = px.bar(DF13,x='Rok',y='Liczba',width=1500,height=500).update_traces(marker_color='rgb(0,70,180)',texttemplate="%{y:}",textposition='inside',marker_line_color='rgb(0,70,180)',marker_line_width=2.5).update_layout(font_family='Lato')
     fig1 = px.bar(DF14,x='Rok',y='Liczba',width=1500,height=500).update_traces(marker_color='rgb(0,70,180)',texttemplate="%{y:}",textposition='inside',marker_line_color='rgb(0,70,180)',marker_line_width=2.5).update_layout(font_family='Lato')
     if kat34 == 'Studia stacjonarne':
