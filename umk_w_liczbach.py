4baa59ad038a70a016a99f8ee75d97f470d3bc65
tomizi <106354750+tomizi@users.noreply.github.com>
2023-01-13 00:22:06 +0100

    Update umk_w_liczbach.py

--- umk_w_liczbach.py
+++ umk_w_liczbach.py
@@ -236,7 +236,39 @@ if sekcja == 'Studenci':
         st.plotly_chart(px.bar(DF20[DF20['Rok']==r],x='Liczba',y='Wydział',orientation='h',barmode='group',color='Rodzaj'))
     with d2:
         st.plotly_chart(px.bar(DF20[DF20['Rok']==r],x='Liczba',y='Wydział',orientation='h'))
+    fig = go.Figure()
+fig.add_trace(go.Bar(x=years,
+			y=[219, 146, 112, 127, 124, 180, 236, 207, 236, 263,
+			   350, 430, 474, 526, 488, 537, 500, 439],
+			name='Rest of world',
+			marker_color='rgb(55, 83, 109)'
+			))
+	fig.add_trace(go.Bar(x=years,
+			y=[16, 13, 10, 11, 28, 37, 43, 55, 56, 88, 105, 156, 270,
+			   299, 340, 403, 549, 499],
+			name='China',
+			marker_color='rgb(26, 118, 255)'
+			))
 
+	fig.update_layout(
+	    title='US Export of Plastic Scrap',
+	    xaxis_tickfont_size=14,
+	    yaxis=dict(
+		title='USD (millions)',
+		titlefont_size=16,
+		tickfont_size=14,
+	    ),
+	    legend=dict(
+		x=0,
+		y=1.0,
+		bgcolor='rgba(255, 255, 255, 0)',
+		bordercolor='rgba(255, 255, 255, 0)'
+	    ),
+	    barmode='group',
+	    bargap=0.15, # gap between bars of adjacent location coordinates.
+	    bargroupgap=0.1 # gap between bars of the same location coordinate.
+	)
+	
     st.header('Porównanie liczby studentów na wybranych wydziałach')
     wydz1 = st.multiselect('Wybierz wydział :  ',DF12['Wydział'].unique())
     st.plotly_chart(px.line(DF15[(DF15['Wydział'].isin(sorted(wydz1)))],x='Rok',y='Liczba',color='Wydział',width=1400,height=500,symbol='Wydział',markers=True,text='Liczba',color_discrete_sequence=list(map(lambda x: kolwyd[x],sorted(wydz1))))
