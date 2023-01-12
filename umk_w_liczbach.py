db473b0d9ee75b22728548924a0ebd00e4063f4d
tomizi <106354750+tomizi@users.noreply.github.com>
2023-01-12 11:25:18 +0100

    Add files via upload

--- umk_w_liczbach.py
+++ umk_w_liczbach.py
@@ -156,24 +156,20 @@ if sekcja == 'Studenci':
    
     st.header('Liczba uczestników studiów w podziale na wydziały')
     q1, q2 = st.columns(2)
-    kat34 = q1.selectbox('Wybierz kategorię : ',['Studia stacjonarne','Studia niestacjonarne','Doktoranckie','Podyplomowe','Razem'])
-    wydzial34 = q2.selectbox('Wybierz wydział : ',DF11['Wydział'].unique())
-	
-    fig = px.bar(DF11[DF11['Wydział']==wydzial34],x='Rok',y='Liczba',width=1500,height=500).update_traces(marker_color='blue',
-    texttemplate="%{y:}",textposition='inside',
-    marker_line_color='rgb(0,70,180)',marker_line_width=2.5).update_layout(font_family='Lato')
-    fig2 = px.bar(DF12,x='Rok',y='Liczba',width=1500,height=500).update_traces(marker_color='rgb(0,70,180)',
-    texttemplate="%{y:}",textposition='inside',
-    marker_line_color='rgb(0,70,180)',marker_line_width=2.5).update_layout(font_family='Lato')
-    fig3 = px.bar(DF10[(DF10['Wydział']==wydzial34) & (DF10['Rodzaj']==kat34)],x='Rok',y='Liczba',width=1500,height=500).update_traces(marker_color='blue',
-	texttemplate="%{y:}",textposition='inside',
-    marker_line_color='rgb(0,70,180)',marker_line_width=2.5).update_layout(font_family='Lato')
+    kat34 = q1.selectbox('Wybierz kategorię : ',['Studia stacjonarne','Studia niestacjonarne','Doktoranckie','Podyplomowe'])
+
+    
+    fig = px.bar(DF12,x='Rok',y='Liczba',width=1500,height=500).update_traces(marker_color='rgb(0,70,180)',texttemplate="%{y:}",textposition='inside',marker_line_color='rgb(0,70,180)',marker_line_width=2.5).update_layout(font_family='Lato')
+    
     if kat34 in ['Studia stacjonarne','Studia niestacjonarne']:
-        st.plotly_chart(fig3)
-    elif kat34 == 'Podyplomowe':
-        st.plotly_chart(fig2)
+        wydzial34 = q2.selectbox('Wybierz wydział : ',DF11['Wydział'])
+        st.plotly_chart(px.bar(DF10[(DF10['Wydział']==wydzial34) & (DF10['Rodzaj']==kat34)],x='Rok',y='Liczba',width=1500,height=500).update_traces(marker_color='blue',texttemplate="%{y:}",textposition='inside',marker_line_color='rgb(0,70,180)',marker_line_width=2.5).update_layout(font_family='Lato'))
     elif kat34 == 'Doktoranckie':
+        wydzial34 = q2.selectbox('Wybierz wydział : ',DF11['Wydział'])
+        st.plotly_chart(px.bar(DF11[DF11['Wydział']==wydzial34],x='Rok',y='Liczba',width=1500,height=500).update_traces(marker_color='blue',texttemplate="%{y:}",textposition='inside',marker_line_color='rgb(0,70,180)',marker_line_width=2.5).update_layout(font_family='Lato'))
+    elif kat34 == 'Podyplomowe':
         st.plotly_chart(fig)
+    
 	
 	
     
