b0c590bb2318f01e51075ba5597d73581c1520c7
tomizi <106354750+tomizi@users.noreply.github.com>
2023-01-12 14:19:32 +0100

    Update umk_w_liczbach.py

--- umk_w_liczbach.py
+++ umk_w_liczbach.py
@@ -196,7 +196,7 @@ if sekcja == 'Studenci':
     wydz1 = r1.selectbox('Wybierz wydział :  ',DF12['Wydział'].unique())
     wydz2 = r2.selectbox('Wybierz wydział  : ',DF12['Wydział'].unique())
     st.dataframe(DF15[DF15['Wydział'].isin([wydz1,wydz2])])
-    st.plotly_chart(px.line(DF15[(DF15['Wydział'].isin([wydz1,wydz2]))],x='Rok',y='Liczba',color='Wydział',width=1400,height=500,markers=True,text=kat).update_traces(marker_color=('rgb(0,80,170)','rgb(250,50,100)'),textposition='top right',texttemplate="%{y:,d}",
+    st.plotly_chart(px.line(DF15[(DF15['Wydział'].isin([wydz1,wydz2]))],x='Rok',y='Liczba',color='Wydział',width=1400,height=500,markers=True,text=y).update_traces(marker_color=('rgb(0,80,170)','rgb(250,50,100)'),textposition='top right',texttemplate="%{y:,d}",
 				line_color=('rgb(0,80,170)')).update_yaxes(tickformat=",").update_layout(font_family='Lato',separators='.,'))
 	
     st.header('Liczba studentów i absolwentów studiów stacjonarnych i niestacjonarnych w latach 2019-2021 na poszczgólnych wydziałach')
