42b531088ec5a759e33423b164442950d31ad001
tomizi <106354750+tomizi@users.noreply.github.com>
2023-01-12 10:42:20 +0100

    Update umk_w_liczbach.py

--- umk_w_liczbach.py
+++ umk_w_liczbach.py
@@ -157,15 +157,15 @@ if sekcja == 'Studenci':
     st.header('Liczba uczestników studiów w podziale na wydziały')
     q1, q2 = st.columns(2)
     kat34 = q1.selectbox('Wybierz kategorię : ',['Studia stacjonarne','Studia niestacjonarne','Doktoranckie','Podyplomowe','Razem'])
-    wydzial34 = q2.selectbox('Wybierz wydział : ',DF11['Wydział'])
+    wydzial34 = q2.selectbox('Wybierz wydział : ',DF12['Wydział'])
 	
-    fig = px.bar(DF11[DF11['Wydział']==wydzial34],x='Rok',y='Liczba',width=550,height=400).update_traces(marker_color=kolwyd[wydzial34],
+    fig = px.bar(DF11[DF11['Wydział']==wydzial34],x='Rok',y='Liczba',width=1500,height=500).update_traces(marker_color=kolwyd[wydzial34],
     texttemplate="%{y:}",textposition='inside',
     marker_line_color='rgb(0,70,180)',marker_line_width=2.5).update_layout(font_family='Lato')
-    fig2 = px.bar(DF12,x='Rok',y='Liczba',width=550,height=400).update_traces(marker_color='rgb(0,70,180)',
+    fig2 = px.bar(DF12,x='Rok',y='Liczba',width=1500,height=500).update_traces(marker_color='rgb(0,70,180)',
     texttemplate="%{y:}",textposition='inside',
     marker_line_color='rgb(0,70,180)',marker_line_width=2.5).update_layout(font_family='Lato')
-    fig3 = px.bar(DF10[(DF10['Wydział']==wydzial34) & (DF10['Rodzaj']==kat34)],x='Rok',y='Liczba',width=550,height=400).update_traces(marker_color=kolwyd[wydzial34],
+    fig3 = px.bar(DF10[(DF10['Wydział']==wydzial34) & (DF10['Rodzaj']==kat34)],x='Rok',y='Liczba',width=1500,height=500).update_traces(marker_color=kolwyd[wydzial34],
 	texttemplate="%{y:}",textposition='inside',
     marker_line_color='rgb(0,70,180)',marker_line_width=2.5).update_layout(font_family='Lato')
     if kat34 in ['Studia stacjonarne','Studia niestacjonarne']:
