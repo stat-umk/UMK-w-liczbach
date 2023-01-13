03249653f14764c39c953397e273e75b84e8ee68
tomizi <106354750+tomizi@users.noreply.github.com>
2023-01-13 16:11:36 +0100

    Update umk_w_liczbach.py

--- umk_w_liczbach.py
+++ umk_w_liczbach.py
@@ -274,13 +274,13 @@ if sekcja == 'Studenci':
     	fig = go.Figure()      
         
     	fig.add_trace(go.Bar(x=y,y=x,orientation='h',text=y,marker_color=barwa4,
-                        textfont=dict( size=12,color='black')).update_traces(marker_line_color='black',marker_line_width=1.5,
+                        textfont=dict( size=12,color='black'),
                       textposition='outside',texttemplate = "<b>Złożone-%{x:}"))
 	    
     	fig.add_trace(go.Bar(x=y1,y=x1,orientation='h',text=y1,marker_color=barwa5,
-                        textfont=dict( size=12,color='black')).update_traces(marker_line_color='black',marker_line_width=1.5,
+                        textfont=dict( size=12,color='black'),
                       textposition='outside',texttemplate = "<b>Przyznane-%{x:}"))
-    	
+    	fig.update_traces(marker_line_color='black',marker_line_width=1.5)
     	fig.update_xaxes(title='Liczba wniosków')
     	fig.update_yaxes(title='Wydział')
     	fig.update_layout(xaxis=dict(showline=False,showgrid=True,showticklabels=True,linewidth=2,linecolor='black',gridwidth=1,gridcolor='gray',mirror=True),
