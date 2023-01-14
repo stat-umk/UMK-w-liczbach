6f92e7426a611a57c565f22f4c928f1adfb41bd6
tomizi <106354750+tomizi@users.noreply.github.com>
2023-01-14 16:58:08 +0100

    Update umk_w_liczbach.py

--- umk_w_liczbach.py
+++ umk_w_liczbach.py
@@ -364,7 +364,7 @@ if sekcja == 'Pracownicy':
     st.plotly_chart(px.line(DF22[DF22['Rodzaj']==pr1],x='Rok',y='Zmiana',color = 'Jednostka',width=1500,height=500,text='Zmiana',color_discrete_sequence=['blue','red','green'])
 		    .update_traces(textposition="top right",texttemplate = "%{y:.2f}%")
 	.update_xaxes(title_font=dict(size=12), title='Rok',range=[2012.95,2021.5],dtick=1)
-		    .update_yaxes(title_font=dict(size=12),title = 'Zmiana liczby pracowników',tickformat=",",range=[-6,6],zeroline=True, zerolinewidth=1, zerolinecolor='rgba(0,0,0,0.5)')                        
+		    .update_yaxes(title_font=dict(size=12),title = 'Zmiana liczby pracowników',tickformat=",",range=[-9,9],zeroline=True, zerolinewidth=1, zerolinecolor='rgba(0,0,0,0.5)')                        
 		    .update_layout(font_family='Lato',separators=','))
 	
 	
