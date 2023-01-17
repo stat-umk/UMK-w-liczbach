4fc9dfeb85d9c5eb3d13cb24fd56b2dc039b1fb4
tomizi <106354750+tomizi@users.noreply.github.com>
2023-01-17 18:49:01 +0100

    Update umk_w_liczbach.py

--- umk_w_liczbach.py
+++ umk_w_liczbach.py
@@ -188,7 +188,7 @@ if sekcja == 'Studenci':
 			.update_traces(marker_color=kolwyd[wydzial34],texttemplate="%{y:}",textposition='inside')
 			.update_xaxes(dtick=1)
 			.update_yaxes(rangemode='tozero',tickformat=" ",title='Liczba studentów')
-			.update_layout(plot_bgcolor='white',font=dict(family='Lato',size=18,color="Black"))
+			.update_layout(plot_bgcolor='white',font=dict(family='Lato',size=18,color="Black")))
     elif kat34 == 'Doktoranckie':
         wydzial34 = q2.selectbox('Wybierz wydział : ',DF12['Wydział'].unique(),index=9)
         st.plotly_chart(px.bar(DF12[DF12['Wydział']==wydzial34],x='Rok',y='Liczba',width=1500,height=500)
