54d05c05f7487c730e6370a52ff82226beba110a
tomizi <106354750+tomizi@users.noreply.github.com>
2023-01-26 20:15:22 +0100

    Update umk_w_liczbach.py

--- umk_w_liczbach.py
+++ umk_w_liczbach.py
@@ -561,7 +561,7 @@ elif sekcja == 'Badania naukowe':
 	    fig.update_yaxes(title='Wydział')
 
 	    fig.update_layout(xaxis=dict(showline=False,showgrid=True,showticklabels=True,linewidth=2,linecolor='black',gridwidth=1,gridcolor='gray',mirror=True),title='<b>Wnioski złożone',title_x=0.5,
-					height=600,width=2000,plot_bgcolor='white',margin=dict(t=100, b=100, l=0, r=200),font=dict(family='Lato',size=18,color="Black"))
+					height=600,width=1600,plot_bgcolor='white',font=dict(family='Lato',size=18,color="Black"))
 
 	    st.plotly_chart(fig,use_container_width=True)
     elif (li == 'Liczba') and (roki in [2019,2020,2021]):
