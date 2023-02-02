a35033a5739d0f3f0a453c55dad9c33c11725e1d
tomizi <106354750+tomizi@users.noreply.github.com>
2023-02-02 19:04:52 +0100

    Update umk_w_liczbach.py

--- umk_w_liczbach.py
+++ umk_w_liczbach.py
@@ -599,7 +599,7 @@ elif sekcja == 'Badania naukowe':
         fig.update_xaxes(title='Kwota wnioskowana[zł]')
         fig.update_yaxes(title='Wydział')
         fig.update_layout(xaxis=dict(showline=False,showgrid=True,showticklabels=True,linewidth=2,linecolor='black',gridwidth=1,gridcolor='gray',mirror=True),title='<b>Wnioski złożone',title_x=0.5,
-        					height=800,width=1600,plot_bgcolor='white',margin=dict(t=200, b=0, l=0, r=100),font=dict(family='Lato',size=18,color="Black"),showlegend=True,legend_orientation='h',legend_x=0,legend_yanchor='top',legend_y=1.2,
+        					height=800,width=1600,plot_bgcolor='white',margin=dict(t=200, b=0, l=0, r=100),font=dict(family='Lato',size=18,color="Black"),showlegend=True,legend_orientation='h',legend_x=-0.1,legend_yanchor='top',legend_y=1.1,
 			 legend_title_text='Rodzaj wniosku')
         
         st.plotly_chart(fig,use_container_width=True)
