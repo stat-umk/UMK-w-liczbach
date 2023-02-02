fad319b897169ef4a4c4a58455910d96a149501a
tomizi <106354750+tomizi@users.noreply.github.com>
2023-02-02 09:54:48 +0100

    Update umk_w_liczbach.py

--- umk_w_liczbach.py
+++ umk_w_liczbach.py
@@ -594,7 +594,7 @@ elif sekcja == 'Badania naukowe':
         fig.add_trace(go.Bar(x=y,y=x,orientation='h',text=y,
         				textfont=dict( size=12,color='black'),marker_color=barwa,marker_line_color='black',marker_line_width=1.5,name='Złożone',marker_pattern_shape="x",
         			      textposition='outside',texttemplate = "<b>%{x:,t}",hovertemplate = 'Wnioski złożone: %{x:,}zł'+"<extra></extra>"))
-        fig.update_xaxes(title='Kwota wnioskowana[zł]',range=[0,y[::-1][0]+y[::-1][0]/4])
+        fig.update_xaxes(title='Kwota wnioskowana[zł]',range=[0,np.max(y,y1)[::-1][0]+np.max(y,y1)[::-1][0]/4])
         fig.update_yaxes(title='Wydział')
         fig.update_layout(xaxis=dict(showline=False,showgrid=True,showticklabels=True,linewidth=2,linecolor='black',gridwidth=1,gridcolor='gray',mirror=True),title='<b>Wnioski złożone',title_x=0.5,
         					height=800,width=1600,plot_bgcolor='white',font=dict(family='Lato',size=18,color="Black"),
@@ -641,7 +641,7 @@ elif sekcja == 'Badania naukowe':
         fig.add_trace(go.Bar(x=y,y=x,orientation='h',text=y,
         				textfont=dict( size=12,color='black'),marker_color=barwa1,marker_line_color='black',marker_line_width=1.5,name='Złożone',marker_pattern_shape="x",
         			      textposition='outside',texttemplate = "<b>%{x:}",hovertemplate = 'Wnioski złożone: %{x:}'+"<extra></extra>"))
-        fig.update_xaxes(title='Liczba wniosków',range=[0,y[::-1][0]+5])
+        fig.update_xaxes(title='Liczba wniosków',range=[0,np.max(y,y1)[::-1][0]+5])
         fig.update_yaxes(title='Wydział')
         
         fig.update_layout(xaxis=dict(showline=False,showgrid=True,showticklabels=True,linewidth=2,linecolor='black',gridwidth=1,gridcolor='gray',mirror=True),title_x=0.5,legend_title_text='Rodzaj wniosku',
@@ -722,7 +722,7 @@ elif sekcja == 'Badania naukowe':
         fig.add_trace(go.Bar(x=y,y=x,orientation='h',text=y,
         				textfont=dict( size=12,color='black'),marker_color=barwa,marker_line_color='black',marker_line_width=1.5,name='Złożone',marker_pattern_shape="x",
         			      textposition='outside',texttemplate = "<b>%{x:,t}",hovertemplate = 'Wnioski złożone: %{x:,}zł'+"<extra></extra>"))
-        fig.update_xaxes(title='Kwota wnioskowana[zł]',range=[0,y[::-1][0]+y[::-1][0]/4])
+        fig.update_xaxes(title='Kwota wnioskowana[zł]',range=[0,np.max(y,y1)[::-1][0]+np.max(y,y1)[::-1][0]/4])
         fig.update_yaxes(title='Wydział')
         
         fig.update_layout(xaxis=dict(showline=False,showgrid=True,showticklabels=True,linewidth=2,linecolor='black',gridwidth=1,gridcolor='gray',mirror=True),title_x=0.5,legend_title_text='Rodzaj wniosku',
@@ -766,7 +766,7 @@ elif sekcja == 'Badania naukowe':
         fig.add_trace(go.Bar(x=y,y=x,orientation='h',text=y,
         				textfont=dict( size=12,color='black'),marker_color=barwa1,marker_line_color='black',marker_line_width=1.5,name='Złożone',marker_pattern_shape="x",
         			      textposition='outside',texttemplate = "<b>%{x:}",hovertemplate = 'Wnioski złożone: %{x:}'+"<extra></extra>"))
-        fig.update_xaxes(title='Liczba wniosków',range=[0,y[::-1][0]+5])
+        fig.update_xaxes(title='Liczba wniosków',range=[0,np.max(y,y1)[::-1][0]+5])
         fig.update_yaxes(title='Wydział')
         
         fig.update_layout(xaxis=dict(showline=False,showgrid=True,showticklabels=True,linewidth=2,linecolor='black',gridwidth=1,gridcolor='gray',mirror=True),title_x=0.5,legend_title_text='Rodzaj wniosku',
@@ -799,7 +799,7 @@ elif sekcja == 'Badania naukowe':
 				textfont=dict( size=12,color='black')))
 	    fig.update_traces(marker_color=barwa,marker_line_color='black',marker_line_width=1.5,
 			      textposition='outside',texttemplate = "<b>%{x:,.2f}%",hovertemplate ='Skuteczność: {x:,.2f}%'+"<extra></extra>")
-	    fig.update_xaxes(title='Skuteczność [%]')
+	    fig.update_xaxes(title='Skuteczność [%]',range=[0,np.max(y)+np.max(y)/5])
 	    fig.update_yaxes(title='Wydział')
 
 	    fig.update_layout(xaxis=dict(showline=False,showgrid=True,showticklabels=True,linewidth=2,linecolor='black',gridwidth=1,gridcolor='gray',mirror=True),title='<b>Współczynnik skuteczności',title_x=0.5,
