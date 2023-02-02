7ea4bc12dea43f11ceb9f428b9a73c692bf10a6e
tomizi <106354750+tomizi@users.noreply.github.com>
2023-02-02 11:31:41 +0100

    Update umk_w_liczbach.py

--- umk_w_liczbach.py
+++ umk_w_liczbach.py
@@ -526,17 +526,7 @@ elif sekcja == 'Pracownicy':
         
         fig55 = px.line(DF30,x='Rok',y='Inflacja',color_discrete_sequence=['red'],markers=True,custom_data=['dr']).update_traces(textfont=dict( size=14),textposition="top left",texttemplate = "%{y:.2f}%",hovertemplate ='<br>Okres: %{customdata}</br>'+'Inflacja w Polsce: %{y:,.2f}%').update_yaxes(
             tickformat=",",showline=False,linewidth=1,gridwidth=1,gridcolor='gray').update_xaxes(
-                showline=True,showticklabels=True,linecolor='gray',linewidth=1,ticks='outside',tickvals=list(range(1,98)),ticktext=['12-2012',
-                                                   '01-2013','03-2013','05-2013','07-2013','09-2013','11-2013',
-                                                   '01-2014','02-2014','03-2014','04-2014','05-2014','06-2014','07-2014','08-2014','09-2014','10-2014','11-2014','12-2014',
-                                                   '01-2015','02-2015','03-2015','04-2015','05-2015','06-2015','07-2015','08-2015','09-2015','10-2015','11-2015','12-2015',
-                                                   '01-2016','02-2016','03-2016','04-2016','05-2016','06-2016','07-2016','08-2016','09-2016','10-2016','11-2016','12-2016',
-                                                   '01-2017','02-2017','03-2017','04-2017','05-2017','06-2017','07-2017','08-2017','09-2017','10-2017','11-2017','12-2017',
-                                                   '01-2018','02-2018','03-2018','04-2018','05-2018','06-2018','07-2018','08-2018','09-2018','10-2018','11-2018','12-2018',
-                                                   '01-2019','02-2019','03-2019','04-2019','05-2019','06-2019','07-2019','08-2019','09-2019','10-2019','11-2019','12-2019',
-                                                   '01-2020','02-2020','03-2020','04-2020','05-2020','06-2020','07-2020','08-2020','09-2020','10-2020','11-2020','12-2020'
-                                                   ],
-                    tickangle=45,range=[0,99]
+                showline=True,showticklabels=True,linecolor='gray',linewidth=1,ticks='outside'
                 ).update_layout(plot_bgcolor='white',font=dict(family='Lato',size=18,color="Black"),separators=',',hovermode='x')
         
         st.plotly_chart(fig55,use_container_width=True)
