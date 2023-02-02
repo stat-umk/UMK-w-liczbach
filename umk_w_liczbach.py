9ed31c15bd870e39400d4a163a31cf6b8089073f
tomizi <106354750+tomizi@users.noreply.github.com>
2023-02-02 18:37:22 +0100

    Update umk_w_liczbach.py

--- umk_w_liczbach.py
+++ umk_w_liczbach.py
@@ -596,10 +596,10 @@ elif sekcja == 'Badania naukowe':
         fig.add_trace(go.Bar(x=y,y=x,orientation='h',text=y,
         				textfont=dict( size=12,color='black'),marker_color=barwa,marker_line_color='black',marker_line_width=1.5,name='Złożone',marker_pattern_shape="x",
         			      textposition='outside',texttemplate = "<b>%{x:,t}",hovertemplate = 'Wnioski złożone: %{x:,}zł'+"<extra></extra>"))
-        fig.update_xaxes(title='Kwota wnioskowana[zł]',range=[0,np.max([y[::-1][0],y[::-1][0]])+np.max([y[::-1][0]/4,y[::-1][0]/4])])
+        fig.update_xaxes(title='Kwota wnioskowana[zł]')
         fig.update_yaxes(title='Wydział')
         fig.update_layout(xaxis=dict(showline=False,showgrid=True,showticklabels=True,linewidth=2,linecolor='black',gridwidth=1,gridcolor='gray',mirror=True),title='<b>Wnioski złożone',title_x=0.5,
-        					height=800,width=1600,plot_bgcolor='white',font=dict(family='Lato',size=18,color="Black"),showlegend=True,
+        					height=800,width=1600,plot_bgcolor='white',margin=dict(t=100, b=100, l=0, r=0),font=dict(family='Lato',size=18,color="Black"),showlegend=True,legend_orientation='x',legend_x=0,legend_y=1,
 			 legend_title_text='Rodzaj wniosku')
         
         st.plotly_chart(fig,use_container_width=True)
