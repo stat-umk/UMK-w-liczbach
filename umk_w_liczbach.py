82b368fadee09540e5b1fa417b3f8e8010c843f9
tomizi <106354750+tomizi@users.noreply.github.com>
2023-02-06 10:25:50 +0100

    Add files via upload

--- umk_w_liczbach.py
+++ umk_w_liczbach.py
@@ -798,19 +798,20 @@ elif sekcja == 'Badania naukowe':
         
             fig = go.Figure()
             fig.add_trace(go.Bar(x=y1,y=x1,orientation='h',
-                                textfont=dict( size=12,color='black'),marker_color=barwa3,marker_line_color='black',marker_line_width=1.5,name='Przyznany',
+                                textfont=dict( size=12,color='black'),marker_color=barwa3,marker_line_color='black',marker_line_width=1.5,
                               hovertemplate = 'Kwota przyznanych grantów: %{x:,}zł'+"<extra></extra>"))
-            
-            fig.add_trace(go.Bar(x=y,y=x,orientation='h',
-            				textfont=dict( size=12,color='black'),marker_color=barwa,marker_line_color='black',marker_line_width=1.5,name='Złożony',marker_pattern_shape="x",
+            st.plotly_chart(fig,use_container_width=True)
+            fig1 = go.Figure()
+            fig1.add_trace(go.Bar(x=y,y=x,orientation='h',
+            				textfont=dict( size=12,color='black'),marker_color=barwa,marker_line_color='black',marker_line_width=1.5,marker_pattern_shape="x",
             			      hovertemplate = 'Kwota wnioskowana: %{x:,}zł'+"<extra></extra>"))
-            fig.update_xaxes(title='Kwota wnioskowana[zł]')
-            fig.update_yaxes(title='Wydział')
+            fig1.update_xaxes(title='Kwota wnioskowana[zł]')
+            fig1.update_yaxes(title='Wydział')
             
-            fig.update_layout(xaxis=dict(showline=False,showgrid=True,showticklabels=True,linewidth=2,linecolor='black',gridwidth=1,gridcolor='gray',mirror=True),title_x=0.5,legend_title_text='Rodzaj wniosku',legend_traceorder='reversed',
+            fig1.update_layout(xaxis=dict(showline=False,showgrid=True,showticklabels=True,linewidth=2,linecolor='black',gridwidth=1,gridcolor='gray',mirror=True),title_x=0.5,legend_title_text='Rodzaj wniosku',legend_traceorder='reversed',
             					height=800,width=1600,plot_bgcolor='white',margin=dict(t=100, b=0, l=180, r=50),font=dict(family='Lato',size=18,color="Black"),showlegend=True,legend_orientation='h',legend_x=-0.1,legend_yanchor='top',legend_y=1.1)
             
-            st.plotly_chart(fig,use_container_width=True)
+            st.plotly_chart(fig1,use_container_width=True)
         elif (li1 == 'Liczba'):
             lw = pd.DataFrame(DF33[DF33['Rok']==roki1].groupby('Jednostka')['Liczba wniosków'].agg(np.sum)).sort_values(by='Liczba wniosków')[::-1]
             x = lw.index[::-1]
