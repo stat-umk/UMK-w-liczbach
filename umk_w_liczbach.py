97413bafaeca2c5c2b9989ef753b9bcb7b2c404d
tomizi <106354750+tomizi@users.noreply.github.com>
2023-02-07 11:45:30 +0100

    Update umk_w_liczbach.py

--- umk_w_liczbach.py
+++ umk_w_liczbach.py
@@ -651,7 +651,7 @@ elif sekcja == 'Badania naukowe':
             kw1 = pd.DataFrame(DF6[DF6['Rok']==roki].groupby('Jednostka')['Kwota przyznana[zł]'].agg(np.sum)).sort_values(by='Kwota przyznana[zł]')[::-1]
             x1 = kw1.index[::-1]+' '
             y1 = kw1['Kwota przyznana[zł]'][::-1]
-	    st.dataframe(y1)
+st.dataframe(y1)
             kw1 = kw1.reset_index()
             kw1['kolor']=' '
             for j,i in enumerate(kw1['Jednostka']):
